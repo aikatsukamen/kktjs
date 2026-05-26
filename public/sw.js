f1e622e5b201d1f908a141a5e852fb3f5279a0ab
aikatsukamen <aikatsukamen@gmail.com>
2026-05-27 00:01:30 +0900

    button

--- public/sw.js
+++ public/sw.js
@@ -5,7 +5,7 @@
 // 例: https://example.com/kktjs/ でも https://example.com/ でも自動追従する。
 const base = self.registration ? self.registration.scope : (location.origin + location.pathname.replace(/[^/]*$/, ''));
 
-const key = "v1.4.8_33";
+const key = "v1.4.8_34";
 const subkey = "?v=0926";
 console.log("sw: new cache! " + key);
 
